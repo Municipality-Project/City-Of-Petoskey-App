9a409b8a41380f8e53e101fe64dad2618512557d
nicolassims <sableye.nico@gmail.com>
2017-04-17 14:38:00 -0400

    Nicolas, beginning work on backend

--- node/DataHandler.js
+++ node/DataHandler.js
@@ -1,21 +1,31 @@
-//   @todo Add todo here
-
 "use strict";
 
-const FS = require ('fs');
+const IO = require('fs');
+
+let userTrips = [];
 
 class DataHandler {
-	constructor() {
-          console.log(`Loading Certificate.`);
-	}
+    static handleUserData(data) {
+        let isTrue = 0;
+        data = JSON.parse(data);
+        data = data.toString().split(/,/);
+        let users = [];
+        let fileReader = IO.readFileSync(`data/users.csv`, `utf8`);
+        let tempArray = fileReader.toString().split(/\r?\n/);
+        for (let i = 0; i < tempArray.length; i++) {
+            users.push(tempArray[i].toString().split(/,/));
+        }
+        for (let i = 0; i < users.length; i++) {
+            if (users[i][0] == data[0] && users[i][1] == data[1]) {
+                isTrue = 1;
+            }
+        }
+        if (isTrue == 1) {
+            return true;
+        }
+    }
 
-     static loadCerts() {
-          const CERTS = {
-               key: FS.readFileSync('./data/certs/server.enc.key'),
-               cert: FS.readFileSync('./data/certs/server.crt')
-          };
-          return CERTS;
-     }
+    //for different methods of handling data, just create a new static method
 }
 
 module.exports = DataHandler;
