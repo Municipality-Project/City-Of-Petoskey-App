d3b7915fb438ed7c16a120648073566e3cd1fc49
nicolassims <sableye.nico@gmail.com>
2017-04-24 14:43:09 -0400

    Re-doing pathing for the new locations. Also, decapilizing hrefs.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ class app {
                } else if (request.url.indexOf('.jpg') >= 0) {
                     this.render(request.url.slice(1), 'image/jpeg', httpHandler, 'binary');
                } else if (request.url.indexOf('/') >= 0) {
-                    this.render('index.ejs', 'text/html', httpHandler, 'utf-8');
+                    this.render('public/views/index.ejs', 'text/html', httpHandler, 'utf-8');
                } else if (request.url.indexOf('.ejs') >= 0) {
                     this.render(request.url.slice(1), 'text/html', httpHandler, 'utf-8');
                } else {
